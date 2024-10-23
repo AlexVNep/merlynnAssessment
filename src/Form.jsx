1e6c33c5a002bd0c167abb897e0747b9da876af9
Alex <alexvnep@gmail.com>
2024-10-23 15:38:37 +0200

    feat: Create test form comp

---
+++ src/Form.jsx
@@ -0,0 +1,12 @@
+function Form() {
+  return (
+    <>
+      <form action="" method="get">
+        <label htmlFor="test">Test: </label>
+        <input type="text" name="test" id="test" />
+      </form>
+    </>
+  );
+}
+
+export default Form;
