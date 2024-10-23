443a42d13545e8611e63dcc050f3919ce2bf6900
Alex <alexvnep@gmail.com>
2024-10-23 15:39:18 +0200

    feat: create greeting comp and add imgContainer comp

---
+++ src/Greeting.jsx
@@ -0,0 +1,15 @@
+import ImgContainer from "./ImgContainer";
+
+function Greeting() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to my TOM API App.</h1>
+        <p>This web app was created using React and Vite</p>
+      </div>
+      <ImgContainer />
+    </>
+  );
+}
+
+export default Greeting;
