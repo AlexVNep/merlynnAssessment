6bd8d1a5b88e048bd2ade723778f2c74066b2459
Alex <alexvnep@gmail.com>
2024-10-23 16:58:23 +0200

    feat: add ListItem comp to create ul

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import ListItem from "./ListItem";
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+}
+
+export default List;
